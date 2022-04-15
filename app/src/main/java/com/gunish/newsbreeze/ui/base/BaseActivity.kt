fc057deb67703668d8316f8492411bd466f08009
gunishmatta <gunishmatta@gmail.com>
2022-04-15 20:56:46 +0530

    added base activity

--- app/src/main/java/com/gunish/newsbreeze/ui/base/BaseActivity.kt
+++ app/src/main/java/com/gunish/newsbreeze/ui/base/BaseActivity.kt
@@ -1,9 +1,19 @@
 package com.gunish.newsbreeze.ui.base
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity:AppCompatActivity() {
+abstract class BaseActivity<VM:ViewModel,VB:ViewBinding>:AppCompatActivity() {
 
+    protected abstract val viewModel:VM
 
+    protected lateinit var mViewBinding:VB
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewBinding=getViewBinding()
+    }
+    abstract fun getViewBinding(): VB
 }
